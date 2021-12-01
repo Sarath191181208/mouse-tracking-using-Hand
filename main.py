3909c6050307440aee9e5bda0d717bcede713c1d
Sarath191181208 <74459981+Sarath191181208@users.noreply.github.com>
2021-12-02 03:12:57 +0530

    Completed the drag function

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from colorama import Fore, Style
 
 from handTracking import HandDetector
 
-# Heads up
+# !! Heads up  !!
 # ** -> power operator in python 
-# // _> integer divisoion in python
+# // -> integer divisoion in python
+# x, y = y, x -> tuple unpacking
+# !! /Heads up !!
+
 TRESHOLD_PARAMS = {
     "left"  : 90,  # Left click treshold
     "right" : 40,  # Right click treshold
-    "scroll": 100  # Scroll up or down treshold
 }
 
+# useful class for later 
 class Finger():
     def __init__(self,is_open, pos) -> None:
         self.x, self.y = int(pos[0]), int(pos[1])
@@ -42,21 +45,33 @@ TRANSLATE_X, TRANSLATE_Y        = (CAM_WIDTH-BOUNDARY_WIDTH)//2, (CAM_HEIGHT-BOU
 # So we just make the capturing rectangle higher 
 TRANSLATE_Y = int(TRANSLATE_Y*SCALE_FACTOR)
 
+# converts a value in a range to a value in another range 
+# useful later
 def interpolate(x_min, x_max, y_min, y_max, num):
     return (((num-x_min)/(x_max-x_min))* (y_max-y_min) ) + y_min
 
+def transform_cord(x, y):
+    # Transforms coordinated to mouse movement onto the screen
+    x-=TRANSLATE_X
+    y-=TRANSLATE_Y
+
+    if x<0 or x>BOUNDARY_WIDTH or y<0 or y>BOUNDARY_HEIGHT:
+        print("outside bounds")
+        return None, None
+
+    # Interpolate converts a range of values of different range of values
+    # MATHS!!!
+    x = interpolate(0, BOUNDARY_WIDTH, 0, WIN_WIDTH, x)
+    y = interpolate(0, BOUNDARY_HEIGHT, 0, WIN_HEIGHT, y)
+
+    return x, y
+
 def move_mouse(x, y):
-        # Interpolate converts a range of values of different range of values
-        # MATHS!!!
-        x-=TRANSLATE_X
-        y-=TRANSLATE_Y
 
-        if x<0 or x>BOUNDARY_WIDTH or y<0 or y>BOUNDARY_HEIGHT:
-            print("outside bounds")
+        x, y = transform_cord(x, y)
+        # Error handling 
+        if x is None or y is None:
             return
-
-        x = interpolate(0, BOUNDARY_WIDTH, 0, WIN_WIDTH, x)
-        y = interpolate(0, BOUNDARY_HEIGHT, 0, WIN_HEIGHT, y)
         # In camera we have mirror image of ourselves  therefore we must 
         # mirror x
         x = WIN_WIDTH-x
@@ -79,30 +94,39 @@ def click(click_btn, x1, y1, x2, y2, img):
     # MATHS !!!
     mid_x, mid_y = (x1+x2)//2, (y1+y2)//2
 
+    clicked = False
     clr = (0, 0, 255)
 
     if dis< TRESHOLD:
         clr = (160, 230, 20)
+        clicked = True
         # TODO: must implement waiting mech or else ...... DOOM!
         mouse.click(click_btn)
 
     cv2.line(img, (x1, y1), (x2, y2), clr, 2)
     cv2.circle(img, (mid_x, mid_y), 10, clr, cv2.FILLED)
 
+    # return true or false if right click or not to make scroll work
+    return clicked
+
 def drag(delta_y):
     mouseX, mouseY = mouse.get_position()
-    mouse.drag(mouseX, mouseY, mouseX, mouseY+(delta_y*TRESHOLD_PARAMS["scroll"]) ,absolute=True, duration=0.5)
+    mouse.drag(mouseX, mouseY, mouseX, delta_y ,absolute=True, duration=0)
 
 
 def main():
 
     detector = HandDetector(detectionCon=0.8)
 
+    # these var's are for drag functionality
+    prev_x, prev_y = 0, 0
+
     while True:
 
         success, img = cap.read()
         # draws the boundary square
         draw_sq(img)
+
         img = detector.findHands(img,draw=True)
         finger_pnts, boundary_box = detector.findPosition(img, handNo=0, draw=False, showNumbers=False)
 
@@ -120,22 +144,31 @@ def main():
 
             no_open_fingers = sum(open_fingers)
 
-            # ---- Moving mouse -----
+            # ----  Moving mouse -----
             if index_finger.is_open and no_open_fingers==1:
                 x, y = index_finger.x, index_finger.y
                 move_mouse(x, y)
-            # ---- Left click   -----
+
+            # ----  Left click   -----
             elif index_finger.is_open and thumb_finger.is_open and no_open_fingers == 2:
                 idx_x, idx_y = index_finger.x, index_finger.y 
                 tmb_x, tmb_y = thumb_finger.x, thumb_finger.y 
                 # Calc dis caluculates the distance between two points given x, y cords of two points
                 # MATHS !!!
                 click("left", idx_x, idx_y, tmb_x, tmb_y, img)
+
             # ----- Right click ------
             elif index_finger.is_open and mid_finger.is_open and no_open_fingers == 2:
+
                 idx_x, idx_y = index_finger.x, index_finger.y 
                 mid_x, mid_y = mid_finger.x, mid_finger.y 
-                click("right", idx_x, idx_y, mid_x, mid_y, img)
+                # if the click was legitemate or not
+                clicked = click("right", idx_x, idx_y, mid_x, mid_y, img)
+
+            # -----    Drag    ------
+                if not clicked:
+                    trans_x, trans_y = transform_cord(idx_x, idx_y )
+                    drag(trans_y)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
